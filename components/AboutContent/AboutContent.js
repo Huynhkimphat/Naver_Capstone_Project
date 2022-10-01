0fe9c30250e50728f269b7e331235777f088cf68
Phat <kimphat.devcs@gmail.com>
2022-10-01 19:23:38 +0700

    Update About Title Com and About Content Com

---
+++ components/AboutContent/AboutContent.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import About from "../../static/about.png";
+
+const styles = {
+  wrapper: "mx-auto flex md:flex-row flex-col",
+  wrapperReverse: "mx-auto  flex md:flex-row-reverse flex-col-reverse",
+  content: "flex flex-col gap-y-6 justify-center md:px-28 p-8",
+  title: "font-semibold text-2xl",
+  labelContainer: "flex flex-col gap-y-6",
+  img: "flex flex-end",
+  logo: "cursor-pointer object-contain",
+  buttonContainer:"flex justify-center md:justify-start",
+  button:
+    "bg-[#F9F9F9] text-center rounded-full w-[10rem] p-2 hover:font-semibold cursor-pointer hover:bg-[#ccc]",
+};
+
+export default function AboutContent({ data }) {
+  return (
+    <div className={data.isReverse ? styles.wrapperReverse : styles.wrapper}>
+      <div className={styles.content}>
+        <div className={styles.title}>{data.title}</div>
+        <div className={styles.labelContainer}>
+          <div className={styles.label}>{data.label1}</div>
+          <div className={styles.label}>{data.label2}</div>
+          <div className={styles.buttonContainer}>
+            <div className={styles.button}>Get in touch</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.img}>
+        <Image className={styles.logo} src={data.logo} alt={""} />
+      </div>
+    </div>
+  );
+}
---
+++ pages/about/index.js
@@ -0,0 +1,51 @@
+import Layout from "../../components/Layout";
+import AboutContent from "../../components/AboutContent/AboutContent";
+import AboutLogo from "../../static/About.png";
+import AboutLogo1 from "../../static/About2.png";
+
+const styles = {
+  wrapper: "mx-auto max-w-7xl flex",
+  content: " ",
+  title: "flex justify-center items-center p-8 text-3xl flex-col",
+};
+
+const aboutLists = [
+  {
+    id: 1,
+    title: `From a studio in London to a global brand with over 400 outlets`,
+    label1: `When we started Avion, the idea was simple. Make high quality furniture affordable and available for the mass market.`,
+    label2: `Handmade, and lovingly crafted furniture and homeware is what we live, breathe and design so our Chelsea boutique become the hotbed for the London interior design community.`,
+    logo: AboutLogo,
+    isReverse: false,
+  },
+  {
+    id: 1,
+    title: `Our service isn’t just personal, it’s actually hyper personally exquisite`,
+    label1: `When we started Avion, the idea was simple. Make high quality furniture affordable and available for the mass market.`,
+    label2: `Handmade, and lovingly crafted furniture and homeware is what we live, breathe and design so our Chelsea boutique become the hotbed for the London interior design community.`,
+    logo: AboutLogo1,
+    isReverse: true,
+  },
+];
+
+export default function About() {
+  return (
+    <Layout>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.title}>
+            <div>A brand built on the love of craftmanship,</div>
+            <div>quality and outstanding customer service</div>
+          </div>
+          <div className={styles.aboutContentContainer}>
+            {aboutLists.map((item) => (
+              <AboutContent key={item.id} data={item} />
+            ))}
+          </div>
+          {/* What... */}
+          {/* Join and feedback */}
+        </div>
+      </div>
+    </Layout>
+  );
+}
