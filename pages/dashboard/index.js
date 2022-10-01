5f8fd5fe1bc778978e5e7cedc30501f3989bab99
doanhnhantre <hieulechanhkk@gmail.com>
2022-10-01 10:49:51 +0700

    Update Adminlayout

---
+++ pages/dashboard/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AdminLayout from '../../components/AdminLayout/AdminLayout'
+const styles = {
+    wrapper: '',
+}
+const index = (props) => {
+    return (
+        <div className={styles.wrapper}>
+            <AdminLayout>
+            </AdminLayout>
+        </div>
+    );
+};
+
+export default index;
