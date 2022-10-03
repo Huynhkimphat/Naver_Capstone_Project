e566223350db6fb9935f8c95f7a63d23f715eb05
huong295 <lethanhhuong2905@gmail.com>
2022-10-03 17:54:21 +0700

    Product detail component update

--- components/ProductDetail/ProductDetail.js
+++ components/ProductDetail/ProductDetail.js
@@ -3,20 +3,33 @@ import ProductDetailImage from "../../static/ProductDetail.png";
 import { HiPlus } from "react-icons/hi";
 import { HiMinus } from "react-icons/hi";
 const styles = {
-  wrapper: "mx-auto flex ",
+  wrapper: "mx-auto flex flex-col sm:flex-row ",
   imageProduct: "",
-  detailContainer: "p-12",
-  
+  detailContainer: "p-2 sm:p-8 m-0 sm:m-12",
+  productTitle: "text-4xl font-sans",
+  productPrice: "text-2xl font-sans py-4 pb-4",
+  descriptionTitle: "text-xl font-sans py-4",
+  descriptionContent: "text-base font-sans",
+  dimensionTitle: "text-xl font-sans py-4",
+  dimensionContent: "text-base font-sans",
+  dimensionTable: "w-sm-table w-56 table-fixed",
+
+  // buy product css
+  buyProduct: "flex justify-between flex-col sm:flex-row ",
+  contentQualityContainer: "flex py-4",
+  quantityAmountTitle: "text-xl font-sans mr-5",
+  contentQuantityAmount: "flex justify-center items-center",
+  btnAddCart: "rounded-lg border p-4 bg-[#2A254B] text-white text-center",
 };
-const ProductDetail = ({ data }) => {
+const ProductDetail = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.imageProduct}>
-        <Image src={ProductDetailImage} width={721} height={759} />
+        <Image src={ProductDetailImage} width={721} height={860} alt="true" />
       </div>
       <div className={styles.detailContainer}>
         <div className={styles.productTitle}>The Dandy Chair</div>
-        <div className={styles.productPrice}>250VND</div>
+        <div className={styles.productPrice}>250000VND</div>
         <div className={styles.descriptionTitle}>Description</div>
         <div className={styles.descriptionContent}>
           A timeless design, with premium materials features as one of our most
@@ -25,36 +38,42 @@ const ProductDetail = ({ data }) => {
         </div>
         <div className={styles.dimensionTitle}>Dimensions</div>
         <div className={styles.dimensionContent}>
-          <table>
-            <tr>
-              <th>Height</th>
-              <th>Width</th>
-              <th>Depth</th>
-            </tr>
-
-            <tr>
-              <td>110</td>
-              <td>75</td>
-              <td>100</td>
-            </tr>
+          <table className={styles.dimensionTable}>
+            <thead>
+              <tr>
+                <td>Width</td>
+                <td>Heigh</td>
+                <td>Depth</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>200</td>
+                <td>150</td>
+                <td>60</td>
+              </tr>
+            </tbody>
           </table>
         </div>
+        {/*buy quality product  */}
+        <div className={styles.buyProduct}>
+          <div className={styles.contentQualityContainer}>
+            <div className={styles.quantityAmountTitle}>Amount</div>
 
-        {/* <div className={styles.buyProduct}>
-        <div className={styles.contentQuantityAmount}>
+            <div className={styles.contentQuantityAmount}>
               <HiMinus className={styles.btnMinus} />
               <input
                 className="mx-2 border text-center w-8"
                 type="text"
-                
                 value={styles.quantityProduct}
               />
               <HiPlus className={styles.btnPus} />
             </div>
-            <div className={styles.btnAddCart}>
-                <button></button>
-            </div>
-        </div> */}
+          </div>
+          <div className={styles.btnAddCart}>
+            <button type="submit">Add to Cart</button>
+          </div>
+        </div>
       </div>
     </div>
   );
