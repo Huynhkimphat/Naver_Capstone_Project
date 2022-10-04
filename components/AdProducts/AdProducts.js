80cfc6dd6e8a3eafb599f713a78d8303194f6115
doanhnhantre <hieulechanhkk@gmail.com>
2022-10-04 08:07:25 +0700

    Update Table

--- components/AdProducts/AdProducts.js
+++ components/AdProducts/AdProducts.js
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import "primeicons/primeicons.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.css";
 import { IoIosAddCircle } from 'react-icons/io'
-import DataTable from 'react-data-table-component';
 import { BiEditAlt } from 'react-icons/bi'
 import { AiOutlineDelete } from 'react-icons/ai'
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { InputText } from "primereact/inputtext";
+import { InputNumber } from "primereact/inputnumber";
+import { Dropdown } from "primereact/dropdown";
+import { Button } from "primereact/button";
+import { Toast } from "primereact/toast";
 const styles = {
     wrapper: 'mx-auto w-full p-4 flex flex-col shadow-lg rounded-md',
     feature: 'w-[100%] flex justify-between items-center gap-4 flex-wrap',
@@ -11,89 +20,124 @@ const styles = {
     btnAdd: 'px-4 py-2 rounded-md bg-[#5842BD] text-white flex items-center gap-2 shadow-lg select-none'
 }
 
-const columns = [
-    {
-        name: 'ID',
-        selector: (row) => row.id,
-        sortable: true,
-        reorder: true
-    },
-    {
-        name: 'Product',
-        selector: (row) => row.product,
-        sortable: true,
-        reorder: true
-    },
-    {
-        name: 'Category',
-        selector: (row) => row.category,
-        sortable: true,
-        reorder: true
-    },
-
-    {
-        name: 'Price',
-        selector: (row) => row.price,
-        sortable: true,
-        reorder: true
-    },
-
-    {
-        name: 'Stock Left',
-        selector: (row) => row.stock,
-        sortable: true,
-        reorder: true
-    },
-    {
-        name: 'Status',
-        selector: (row) => row.status,
-    },
-    {
-        name: '',
-        selector: (row) => row.action,
-    },
-];
-const data = [
-    {
-        id: 1,
-        product: 'Gray Sofa',
-        category: 'Decor items',
-        price: '$137.00',
-        stock: '20',
-        status: <div className='w-full flex justify-around items-center gap-2'>
-                    <div className='w-2 h-2 bg-green-500 rounded-[50%]'></div>
-                    <span>In Stock</span>
-                </div>,
-        action: <div className='w-full flex justify-around item-center gap-2'>
-                    <BiEditAlt className='cursor-pointer'></BiEditAlt>
-                    <AiOutlineDelete className='cursor-pointer'></AiOutlineDelete>
-                </div>
-    },
-    {
-        id: 2,
-        product: 'Terrible Chair',
-        category: 'Chair',
-        price: '$20.00',
-        stock: '0',
-        status: <div className='w-full flex justify-around items-center gap-2'>
-                    <div className='w-2 h-2 bg-red-500 rounded-[50%]'></div>
-                    <span>Out of Stock</span>
-                </div>,
-        action: <div className='w-full flex justify-around item-center gap-2'>
-                    <BiEditAlt className='cursor-pointer'></BiEditAlt>
-                    <AiOutlineDelete className='cursor-pointer'></AiOutlineDelete>
-                </div>
-    },
-];
 
 const AdProducts = (props) => {
 
-    const [hasWindow, setHasWindow] = useState(false);
+    const [products2, setProducts2] = useState(null);
+    const statuses = [
+        { label: "In Stock", value: "INSTOCK" },
+        { label: "Low Stock", value: "LOWSTOCK" },
+        { label: "Out of Stock", value: "OUTOFSTOCK" }
+    ];
+    const dataTableFuncMap = {
+        products2: setProducts2,
+    };
     useEffect(() => {
-        if (typeof window !== "undefined") {
-            setHasWindow(true);
-        }
+        setProducts2([
+            {
+                id: "1000",
+                code: "f230fh0g3",
+                name: "Bamboo Watch",
+                description: "Product Description",
+                image: "bamboo-watch.jpg",
+                price: 65,
+                category: "Accessories",
+                quantity: 24,
+                inventoryStatus: "INSTOCK",
+                rating: 5,
+            },
+            {
+                id: "1001",
+                code: "nvklal433",
+                name: "Black Watch",
+                description: "Product Description",
+                image: "black-watch.jpg",
+                price: 72,
+                category: "Accessories",
+                quantity: 61,
+                inventoryStatus: "OUTOFSTOCK",
+                rating: 4,
+            }
+        ])
     }, []);
+
+    const getStatusLabel = (status) => {
+        switch (status) {
+            case "INSTOCK":
+                return "In Stock";
+
+            case "LOWSTOCK":
+                return "Low Stock";
+
+            case "OUTOFSTOCK":
+                return "Out of Stock";
+
+            default:
+                return "NA";
+        }
+    };
+    const onRowEditComplete = (e) => {
+        let _products2 = [...products2];
+        let { newData, index } = e;
+
+        _products2[index] = newData;
+
+        setProducts2(_products2);
+    };
+
+    const textEditor = (options) => {
+        return (
+            <InputText
+                type="text"
+                value={options.value}
+                onChange={(e) => options.editorCallback(e.target.value)}
+            />
+        );
+    };
+    const statusEditor = (options) => {
+        return (
+            <Dropdown
+                value={options.value}
+                options={statuses}
+                optionLabel="label"
+                optionValue="value"
+                onChange={(e) => options.editorCallback(e.value)}
+                placeholder="Select a Status"
+                itemTemplate={(option) => {
+                    return (
+                        <span
+                            className={`product-badge status-${option.value.toLowerCase()}`}
+                        >
+                            {option.label}
+                        </span>
+                    );
+                }}
+            />
+        );
+    };
+
+    const priceEditor = (options) => {
+        return (
+            <InputNumber
+                value={options.value}
+                onValueChange={(e) => options.editorCallback(e.value)}
+                mode="currency"
+                currency="USD"
+                locale="en-US"
+            />
+        );
+    };
+
+    const statusBodyTemplate = (rowData) => {
+        return getStatusLabel(rowData.inventoryStatus);
+    };
+
+    const priceBodyTemplate = (rowData) => {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(rowData.price);
+    };
     return (
         <div className={styles.wrapper}>
             <div className={styles.feature}>
@@ -119,13 +163,55 @@ const AdProducts = (props) => {
                 </button>
             </div>
             <div>
-                {hasWindow && <DataTable
-                    columns={columns}
-                    data={data}
-                    responsive
-                    pagination
-                    selectableRows
-                />}
+                <DataTable
+                    value={products2}
+                    editMode="row"
+                    dataKey="id"
+                    onRowEditComplete={onRowEditComplete}
+                    responsiveLayout="scroll"
+                >
+                    <Column
+                        field="id"
+                        header="ID"
+                        sortable
+                        editor={(options) => textEditor(options)}
+                        style={{ width: "15%" }}
+                    ></Column>
+                    <Column
+                        field="name"
+                        header="Name"
+                        sortable
+                        editor={(options) => textEditor(options)}
+                        style={{ width: "15%" }}
+                    ></Column>
+                    <Column
+                        field="category"
+                        header="Category"
+                        sortable
+                        style={{ width: "15%" }}
+                    ></Column>
+                    <Column
+                        field="inventoryStatus"
+                        header="Status"
+                        sortable
+                        body={statusBodyTemplate}
+                        editor={(options) => statusEditor(options)}
+                        style={{ width: "15%" }}
+                    ></Column>
+                    <Column
+                        field="price"
+                        header="Price"
+                        sortable
+                        body={priceBodyTemplate}
+                        editor={(options) => priceEditor(options)}
+                        style={{ width: "15%" }}
+                    ></Column>
+                    <Column
+                        rowEditor
+                        headerStyle={{ width: "10%", minWidth: "8rem" }}
+                        bodyStyle={{ textAlign: "center" }}
+                    ></Column>
+                </DataTable>
             </div>
         </div>
     );
--- components/AdminOrders/AdminOrders.js
+++ components/AdminOrders/AdminOrders.js
@@ -1,219 +1,148 @@
-import React, { useEffect, useState } from 'react';
+import "primeicons/primeicons.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.css";
+import React, { useEffect, useState, useRef } from 'react';
 import { FaFileExcel } from 'react-icons/fa'
-import DataTable from 'react-data-table-component';
+// import DataTable from 'react-data-table-component';
 import { BiEditAlt } from 'react-icons/bi'
 import { AiOutlineDelete } from 'react-icons/ai'
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { InputText } from "primereact/inputtext";
+import { InputNumber } from "primereact/inputnumber";
+import { Dropdown } from "primereact/dropdown";
+import { Button } from "primereact/button";
+import { Toast } from "primereact/toast";
 const styles = {
     wrapper: 'mx-auto w-full p-4 flex flex-col shadow-lg rounded-md',
     exportTable: 'w-[100%] flex justify-between items-center',
     select: 'px-4 py-2 rounded-md shadow-lg',
     btnExport: 'px-4 py-2 rounded-md bg-[#5842BD] text-white flex items-center gap-2 shadow-lg'
 }
-const columns = [
-    {
-        name: 'ID',
-        selector: (row) => row.id,
-        sortable: true,
-        reorder: true
-    },
-    {
-        name: 'Customer',
-        selector: (row) => row.customer,
-        sortable: true,
-        reorder: true
-    },
-    {
-        name: 'Date',
-        selector: (row) => row.date,
-        sortable: true,
-        reorder: true
-    },
-
-    {
-        name: 'Amount',
-        selector: (row) => row.amount,
-        sortable: true,
-        reorder: true
-    },
-
-    {
-        name: 'Status',
-        selector: (row) => row.status,
-        sortable: true,
-        reorder: true
-    },
-    {
-        name: 'Action',
-        selector: (row) => row.action,
-    },
-];
-const data = [
-    {
-        id: 1,
-        customer: 'Beetlejuice',
-        date: '1988',
-        amount: '$137.00',
-        status: <div className='w-full flex justify-around items-center gap-2'>
-                    <div className='w-2 h-2 bg-green-500 rounded-[50%]'></div>
-                    <span>Approved</span>
-                </div>,
-        action: <div className='w-full flex justify-around item-center gap-2'>
-                    <BiEditAlt className='cursor-pointer'></BiEditAlt>
-                    <AiOutlineDelete className='cursor-pointer'></AiOutlineDelete>
-                </div>
-    },
-    {
-        id: 2,
-        customer: 'Ghostbusters',
-        date: '1984',
-        amount: '$137.00',
-        status: <div className='w-full flex justify-around items-center gap-2'>
-                    <div className='w-2 h-2 bg-green-500 rounded-[50%]'></div>
-                    <span>Approved</span>
-                </div>,
-        action: <div className='w-full flex justify-around item-center gap-2'>
-                    <BiEditAlt className='cursor-pointer'></BiEditAlt>
-                    <AiOutlineDelete className='cursor-pointer'></AiOutlineDelete>
-                </div>
-    },
-    {
-        id: 3,
-        customer: 'Beetlejuice',
-        date: '1988',
-        amount: '$137.00',
-        status: <div className='w-full flex justify-around items-center gap-2'>
-                    <div className='w-2 h-2 bg-green-500 rounded-[50%]'></div>
-                    <span>Approved</span>
-                </div>,
-        action: <div className='w-full flex justify-around item-center gap-2'>
-                    <BiEditAlt className='cursor-pointer'></BiEditAlt>
-                    <AiOutlineDelete className='cursor-pointer'></AiOutlineDelete>
-                </div>
-    },
-    {
-        id: 4,
-        customer: 'Ghostbusters',
-        date: '1984',
-        amount: '$137.00',
-        status: <div className='w-full flex justify-around items-center gap-2'>
-                    <div className='w-2 h-2 bg-green-500 rounded-[50%]'></div>
-                    <span>Approved</span>
-                </div>,
-        action: <div className='w-full flex justify-around item-center gap-2'>
-                    <BiEditAlt className='cursor-pointer'></BiEditAlt>
-                    <AiOutlineDelete className='cursor-pointer'></AiOutlineDelete>
-                </div>
-    },
-    {
-        id: 5,
-        customer: 'Beetlejuice',
-        date: '1988',
-        amount: '$137.00',
-        status: <div className='w-full flex justify-around items-center gap-2'>
-                    <div className='w-2 h-2 bg-green-500 rounded-[50%]'></div>
-                    <span>Approved</span>
-                </div>,
-        action: <div className='w-full flex justify-around item-center gap-2'>
-                    <BiEditAlt className='cursor-pointer'></BiEditAlt>
-                    <AiOutlineDelete className='cursor-pointer'></AiOutlineDelete>
-                </div>
-    },
-    {
-        id: 6,
-        customer: 'Ghostbusters',
-        date: '1984',
-        amount: '$137.00',
-        status: <div className='w-full flex justify-around items-center gap-2'>
-                    <div className='w-2 h-2 bg-green-500 rounded-[50%]'></div>
-                    <span>Approved</span>
-                </div>,
-        action: <div className='w-full flex justify-around item-center gap-2'>
-                    <BiEditAlt className='cursor-pointer'></BiEditAlt>
-                    <AiOutlineDelete className='cursor-pointer'></AiOutlineDelete>
-                </div>
-    },
-    {
-        id: 7,
-        customer: 'Beetlejuice',
-        date: '1988',
-        amount: '$137.00',
-        status: <div className='w-full flex justify-around items-center gap-2'>
-                    <div className='w-2 h-2 bg-green-500 rounded-[50%]'></div>
-                    <span>Approved</span>
-                </div>,
-        action: <div className='w-full flex justify-around item-center gap-2'>
-                    <BiEditAlt className='cursor-pointer'></BiEditAlt>
-                    <AiOutlineDelete className='cursor-pointer'></AiOutlineDelete>
-                </div>
-    },
-    {
-        id: 8,
-        customer: 'Ghostbusters',
-        date: '1984',
-        amount: '$137.00',
-        status: <div className='w-full flex justify-around items-center gap-2'>
-                    <div className='w-2 h-2 bg-green-500 rounded-[50%]'></div>
-                    <span>Approved</span>
-                </div>,
-        action: <div className='w-full flex justify-around item-center gap-2'>
-                    <BiEditAlt className='cursor-pointer'></BiEditAlt>
-                    <AiOutlineDelete className='cursor-pointer'></AiOutlineDelete>
-                </div>
-    },
-    {
-        id: 9,
-        customer: 'Beetlejuice',
-        date: '1988',
-        amount: '$137.00',
-        status: <div className='w-full flex justify-around items-center gap-2'>
-                    <div className='w-2 h-2 bg-green-500 rounded-[50%]'></div>
-                    <span>Approved</span>
-                </div>,
-        action: <div className='w-full flex justify-around item-center gap-2'>
-                    <BiEditAlt className='cursor-pointer'></BiEditAlt>
-                    <AiOutlineDelete className='cursor-pointer'></AiOutlineDelete>
-                </div>
-    },
-    {
-        id: 10,
-        customer: 'Ghostbusters',
-        date: '1984',
-        amount: '$137.00',
-        status: <div className='w-full flex justify-around items-center gap-2'>
-                    <div className='w-2 h-2 bg-green-500 rounded-[50%]'></div>
-                    <span>Approved</span>
-                </div>,
-        action: <div className='w-full flex justify-around item-center gap-2'>
-                    <BiEditAlt className='cursor-pointer'></BiEditAlt>
-                    <AiOutlineDelete className='cursor-pointer'></AiOutlineDelete>
-                </div>
-    },
-    {
-        id: 11,
-        customer: 'Ghostbusters',
-        date: '1984',
-        amount: '$137.00',
-        status: <div className='w-full flex justify-around items-center gap-2'>
-                    <div className='w-2 h-2 bg-green-500 rounded-[50%]'></div>
-                    <span>Approved</span>
-                </div>,
-        action: <div className='w-full flex justify-around item-center gap-2'>
-                    <BiEditAlt className='cursor-pointer'></BiEditAlt>
-                    <AiOutlineDelete className='cursor-pointer'></AiOutlineDelete>
-                </div>
-    },
-];
 
 const AdminOrders = (props) => {
-    const [hasWindow, setHasWindow] = useState(false);
+    const [products2, setProducts2] = useState(null);
+    const statuses = [
+        { label: "Approved", value: "APPROVED" },
+        { label: "Pending", value: "PENDING" },
+        { label: "Reject", value: "REJECT" }
+    ];
+    const dataTableFuncMap = {
+        products2: setProducts2,
+    };
     useEffect(() => {
-        if (typeof window !== "undefined") {
-            setHasWindow(true);
-        }
+        setProducts2([
+            {
+                id: "1000",
+                code: "f230fh0g3",
+                name: "Bamboo Watch",
+                description: "Product Description",
+                image: "bamboo-watch.jpg",
+                date: '1998',
+                price: 65,
+                category: "Accessories",
+                quantity: 24,
+                inventoryStatus: "INSTOCK",
+                rating: 5,
+                orderstatus:'APPROVED'
+            },
+            {
+                id: "1001",
+                code: "nvklal433",
+                name: "Black Watch",
+                description: "Product Description",
+                image: "black-watch.jpg",
+                date: "1997",
+                price: 72,
+                category: "Accessories",
+                quantity: 61,
+                inventoryStatus: "OUTOFSTOCK",
+                rating: 4,
+                orderstatus:'REJECT'
+            }
+        ])
     }, []);
+
+    const getStatusLabel = (status) => {
+        switch (status) {
+          case "APPROVED":
+            return "Approved";
+    
+          case "REJECT":
+            return "Reject";
+    
+          case "PENDING":
+            return "Pending";
+    
+          default:
+            return "NA";
+        }
+      };
+    const onRowEditComplete = (e) => {
+        let _products2 = [...products2];
+        let { newData, index } = e;
+
+        _products2[index] = newData;
+
+        setProducts2(_products2);
+    };
+
+    const textEditor = (options) => {
+        return (
+            <InputText
+                type="text"
+                value={options.value}
+                onChange={(e) => options.editorCallback(e.target.value)}
+            />
+        );
+    };
+    const statusEditor = (options) => {
+        return (
+            <Dropdown
+                value={options.value}
+                options={statuses}
+                optionLabel="label"
+                optionValue="value"
+                onChange={(e) => options.editorCallback(e.value)}
+                placeholder="Select a Status"
+                itemTemplate={(option) => {
+                    return (
+                        <span
+                            className={`product-badge status-${option.value.toLowerCase()}`}
+                        >
+                            {option.label}
+                        </span>
+                    );
+                }}
+            />
+        );
+    };
+    
+    const priceEditor = (options) => {
+        return (
+            <InputNumber
+                value={options.value}
+                onValueChange={(e) => options.editorCallback(e.value)}
+                mode="currency"
+                currency="USD"
+                locale="en-US"
+            />
+        );
+    };
+
+    const statusBodyTemplate = (rowData) => {
+        return getStatusLabel(rowData.orderstatus);
+    };
+
+    const priceBodyTemplate = (rowData) => {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(rowData.price);
+    };
     return (
         // Export File Excel
-        <div className={styles.wrapper}> 
+        <div className={styles.wrapper}>
             <div className={styles.exportTable}>
                 <select className={styles.select} defaultValue='All'>
                     <option value="All">All</option>
@@ -228,13 +157,55 @@ const AdminOrders = (props) => {
             </div>
             {/* Orders List Table */}
             <div>
-                {hasWindow && <DataTable
-                    columns={columns}
-                    data={data}
-                    responsive
-                    pagination
-                    selectableRows
-                />}
+                <DataTable
+                    value={products2}
+                    editMode="row"
+                    dataKey="id"
+                    onRowEditComplete={onRowEditComplete}
+                    responsiveLayout="scroll"
+                >
+                    <Column
+                        field="code"
+                        header="Code"
+                        sortable
+                        editor={(options) => textEditor(options)}
+                        style={{ width: "20%" }}
+                    ></Column>
+                    <Column
+                        field="name"
+                        header="Name"
+                        sortable
+                        editor={(options) => textEditor(options)}
+                        style={{ width: "20%" }}
+                    ></Column>
+                    <Column
+                        field="date"
+                        header="Date"
+                        sortable
+                        style={{ width: "20%" }}
+                    ></Column>
+                    <Column
+                        field="price"
+                        header="Amount"
+                        sortable
+                        body={priceBodyTemplate}
+                        editor={(options) => priceEditor(options)}
+                        style={{ width: "20%" }}
+                    ></Column>
+                    <Column
+                        field="orderstatus"
+                        header="Status"
+                        sortable
+                        body={statusBodyTemplate}
+                        editor={(options) => statusEditor(options)}
+                        style={{ width: "20%" }}
+                    ></Column>
+                    <Column
+                        rowEditor
+                        headerStyle={{ width: "10%", minWidth: "8rem" }}
+                        bodyStyle={{ textAlign: "center" }}
+                    ></Column>
+                </DataTable>
             </div>
         </div>
     );
