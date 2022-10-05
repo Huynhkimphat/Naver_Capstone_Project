33f2ee6ff41119da3e00ff4041026e75de62e6b8
tranthidiep142 <tranthidiep142@gmail.com>
2022-10-05 15:40:50 +0700

    fixed button

--- components/HeaderBanner/HeaderBanner.js
+++ components/HeaderBanner/HeaderBanner.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Image from "next/image";
-import HeaderBannerImg from "../../static/headerBanner.jpg";
+import Product1 from "../../static/Product1.png";
 
 const styles = {
-  wrapper: "flex justify-center lg:px-20 lg:pt-14 text-white font-normal pb-10",
-  contentContainer: "w-full md:w-4/12 bg-[#2A254B] lg:p-10 p-6",
+  wrapper: "flex lg:p-14 md:p-12 text-white font-normal",
+  contentContainer: "w-full md:w-8/12 bg-[#2A254B] lg:p-10 p-6",
   title: "lg:w-3/4 text-3xl",
   mainContent: "flex flex-col lg:flex-col-reverse",
   description: "mt-20 lg:mt-30 md:mt-15 mb-8 lg:mb-0",
-  btnViewCollection:
-    "bg-[#F9F9F926] lg:px-8 lg:py-4 p-2 lg:mt-10 text-center md:w-8/12 w-full",
-  imgContainer: "md:w-8/12 w-0 relative",
+  btnViewCollection: "bg-[#F9F9F926] lg:mt-10 py-4 px-8 w-full md:w-auto",
+  imgContainer: "md:w-4/12 w-0 relative",
 };
 
 const HeaderBanner = () => {
@@ -26,16 +25,13 @@ const HeaderBanner = () => {
             retail brand with nice fonts, tasteful colors and a beautiful way to
             display things digitally using modern web technologies.
           </div>
-          <button className={styles.btnViewCollection}>View collection</button>
+          <div>
+            <button className={styles.btnViewCollection}>View collection</button>
+          </div>
         </div>
       </div>
       <div className={styles.imgContainer}>
-        <Image
-          className={styles.img}
-          src={HeaderBannerImg}
-          alt={""}
-          layout="fill"
-        />
+        <Image src={Product1} alt={""} layout="fill"/>
       </div>
     </div>
   );
