606e69221c0ed245ba6148f3671e891d66b0f541
Nguyễn Đức Hiếu <65936332+hieulechanhkk@users.noreply.github.com>
2022-10-13 19:54:30 +0700

    Update Link images in Firestore (#66)

--- services/api/admin/productService.js
+++ services/api/admin/productService.js
@@ -1,17 +1,35 @@
-import { setDoc, doc, collection } from "firebase/firestore";
+import { setDoc, doc, collection, updateDoc, FieldPath, arrayUnion } from "firebase/firestore";
 import { db, storage } from "../../../lib/firebase";
-import { ref, uploadBytesResumable } from "firebase/storage"
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage"
+import { async } from "@firebase/util";
 
 const productService = {
     async addProduct(data, images) {
         const newDocRef = doc(collection(db, "product"));
         await setDoc(newDocRef, data);
-
         // Upload image
         images.forEach((image, index) => {
             const imgType = image.type.split("/")[1];
             const storageRef = ref(storage, `product/${newDocRef.id}/product_${index}.${imgType}`);
-            uploadBytesResumable(storageRef, image)
+            const uploadTask = uploadBytesResumable(storageRef, image)
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    const prog = Math.round(
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                    );
+                },
+                (error) => console.log(error),
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref)
+                        .then(async (url) => {
+                            const imgName = `img_${index}`;
+                            await updateDoc(newDocRef, {
+                                images: arrayUnion(url)
+                            })
+                        })
+                }
+            )
         });
     },
 }
