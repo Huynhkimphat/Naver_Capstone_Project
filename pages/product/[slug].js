8e970710e9fb18bf155724eb8dd4f4ac15afbc8c
Phat <kimphat.devcs@gmail.com>
2022-10-14 19:03:03 +0700

    Update Product Detail Flow

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -6,26 +6,40 @@ import Banner from "../../components/Banner/Banner";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 import AppSelector from "../../redux/selector";
+import { Puff } from "react-loader-spinner";
 
 const styles = {
-  wrapper: "container mx-auto mt-10",
+    wrapper: "container mx-auto mt-10",
 };
 
 export default function ProductDetailPage() {
-  const router = useRouter();
+    const router = useRouter();
 
-  const productId = router.query.slug;
-  const productList = useSelector((state) => AppSelector.getProduct(state));
-  const product = productList.filter((item) => item.id === productId);
+    const productId = router.query.slug;
+    const productList = useSelector((state) => AppSelector.getProduct(state));
+    const product = productList.filter((item) => item.id === productId);
 
-  return (
-    <Layout>
-      <div className={styles.wrapper}>
-        <ProductDetail product={product} />
-        <ProductList />
-        <AttentionContent />
-        <Banner />
-      </div>
-    </Layout>
-  );
+    return (
+        <Layout>
+            {router.query.slug ? (
+                <div className={styles.wrapper}>
+                    <ProductDetail product={product} />
+                    <ProductList />
+                    <AttentionContent />
+                    <Banner />
+                </div>
+            ) : (
+                <Puff
+                    height="80"
+                    width="80"
+                    radisu={1}
+                    color="#4fa94d"
+                    ariaLabel="puff-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    visible={true}
+                />
+            )}
+        </Layout>
+    );
 }
