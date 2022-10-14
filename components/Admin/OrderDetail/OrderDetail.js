3980dc24f47ba593a43a57f849cfc7fbfdabc84f
Nguyễn Đức Hiếu <65936332+hieulechanhkk@users.noreply.github.com>
2022-10-14 20:17:41 +0700

    Feature/order detail (#72)
    * Update change approve & reject order detail
    
    * Update Order Detail

--- components/Admin/OrderDetail/OrderDetail.js
+++ components/Admin/OrderDetail/OrderDetail.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image'
 import _Image from '../../../static/Product1.png'
 import { useDispatch, useSelector } from 'react-redux';
 import updateField from '../../../services/api/admin/updateField';
 import { updateStatus } from '../../../redux/actions/orderAction';
 import { useRouter } from 'next/router';
+import userService from '../../../services/api/userService';
+import productService from '../../../services/api/admin/productService';
+
 const styles = {
     wrapper: 'mx-auto w-full p-4 flex flex-col mt-4 rounded-lg',
     header: 'bg-admin_color flex p-4 justify-between border-b-2 rounded-lg shadow-xl',
@@ -22,34 +25,43 @@ const styles = {
     approved: "bg-green-500 hover:bg-green-600",
     reject: "bg-red-500 hover:bg-red-600",
     unableClick: "pointer-events-none bg-gray-500",
-    productContainer: 'w-full flex flex-col gap-2 mt-4 border-b-2 pb-4',
+    productContainer: 'w-full flex justify-between p-4 gap-8 items-center mt-4 border-b-2 pb-4',
     productName: 'font-semibold text-xl',
     btnGroup: 'flex gap-4 justify-end mt-8',
-    
+    productItInfo: 'font-semibold',
 
 }
 const OrderDetail = () => {
     // useSelector to select state in store, it references to state.order
     const dispatch = useDispatch();
     const router = useRouter();
+    const [products, setProducts] = useState([])
+    const [user, setUser] = useState({})
     const {
         code,
+        customerId,
         date,
         totalPrice,
         status,
         productListDetail
     } = useSelector(state => state.rootReducer.order.data)
     useEffect(() => {
-        console.log(productListDetail);
+        userService.getUserByEmail(customerId).then(res => setUser(res));
+        productService.getProductsByIds(productListDetail).then(res => setProducts(res))
     }, [])
     const ordersList = productListDetail.map((order, index) => {
         return (
             <div key={order.productId}
-                className={styles.productContainer}>
-                <h1 className={styles.productName}>Headphones Bose 35 II</h1>
-                <span>Qt: {order.amount} Item</span>
-                <h1 className={styles.productName}>Price: {order.total} VNĐ via (COD)</h1>
-                <span>Tracking Status on: 11:30PM, Today</span>
+                className={styles.productContainer}>   
+                <div className='flex flex-col gap-2'>
+                    <h1 className={styles.productName}>Name: {products[index]?.name}</h1>
+                    <span className={styles.productItInfo}>Category: {products[index]?.category}</span>
+                    <span className={styles.productItInfo}>Price: {products[index]?.price} VNĐ</span>
+                    <span className={styles.productItInfo}>Qt: {order.amount} Item</span>
+                    <h1 className={styles.productName}>Total: {order.total} VNĐ via (COD)</h1>
+                    <span>Tracking Status on: 11:30PM, Today</span>
+                </div>
+                <Image className='rounded-2xl' src={products[index].images[0]} width={200} height={200} alt=""></Image>
             </div>
         )
     })
@@ -73,11 +85,11 @@ const OrderDetail = () => {
             <div className={styles.customerContainer}>
                 <div className={styles.customerField}>
                     <div className={styles.customerInfo}>
-                        <h1 className={styles.customerTitle}>Customer: Nguyen Hieu</h1>
-                        <span>Customer ID: 9239293</span>
-                        <span>Email: hieulechanhkk@gmail.com</span>
-                        <span>Phone: 0913906117</span>
-                        <span>Address: HCM</span>
+                        <h1 className={styles.customerTitle}>Customer: {user.name}</h1>
+                        <span>Customer ID: {customerId}</span>
+                        <span>Email: {user.email}</span>
+                        <span>Phone: {!user.phone ? "NA" : user.phone}</span>
+                        <span>Address: {!user.address ? "NA" : user.address}</span>
                     </div>
                 </div>
                 <div>
