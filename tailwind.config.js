e1a17c3f99842d27ecd96f74d0b8da1f49dcae5d
doanhnhantre <hieulechanhkk@gmail.com>
2022-10-14 15:31:07 +0700

    Update change approve & reject order detail

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
       colors: {
         light_blue: 'rgba(249, 249, 249, 0.15);',
         dark_blue: '#2A254B',
-        admin_color: '#5842BD'
+        admin_color: '#5842BD',
+        admin_second_color: "#3AA1E9"
       }
     },
   },
