3a8c90b04ceb58aa9c22056ed13063a6400c6f7d
Nguyễn Đức Hiếu <65936332+hieulechanhkk@users.noreply.github.com>
2022-10-15 10:33:03 +0700

    Update Feature Analyst Line & Dou Chart (#78)

--- components/Admin/Charts/LineChart/LineChart.js
+++ components/Admin/Charts/LineChart/LineChart.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Line } from "react-chartjs-2";
 import Chart from "chart.js/auto";
+import { useSelector } from 'react-redux';
 Chart.defaults.scale.grid.display = false;
 const styles = {
     lineChart: "w-full p-4 shadow-lg sm:w-[65%]",
@@ -9,20 +10,39 @@ const styles = {
         "p-1 bg-[#5842BD] text-white font-medium rounded-md shadow-2xl",
 }
 
-const data = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "August"],
-    datasets: [
-        {
-            label: "Total Orders",
-            data: [33, 53, 85, 41, 44, 65, 90, 60],
-            fill: true,
-            backgroundColor: "rgba(75,192,192,0.2)",
-            borderColor: "rgba(75,192,192,1)",
-            lineTension: 0.3,
-        },
-    ],
-};
+
 const LineChart = (props) => {
+    const orders = useSelector(state => state.rootReducer.order.orderList)
+    const [dataLine, setDataLine] = useState({
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        datasets: [
+        ],
+    })
+    const getDataDateOrder = dataLine.labels.map((month, index) => {
+        return orders.filter((order) => {
+            return order.date.split("/")[1] == index + 1
+        }).length;
+    })
+    const getMonth = () => {
+        let newDate = new Date();
+        return dataLine.labels.slice(0,newDate.getMonth()+1)
+    }
+    useEffect(() => {
+        setDataLine(
+            {
+                labels: getMonth(),
+                datasets: [
+                    {
+                        label: "Total Orders",
+                        data: getDataDateOrder,
+                        fill: true,
+                        backgroundColor: "rgba(75,192,192,0.2)",
+                        borderColor: "rgba(75,192,192,1)",
+                        lineTension: 0.3,
+                    },
+                ],
+            })
+    }, [])
     return (
         <div className={styles.lineChart}>
             <div className={styles.titleLineChart}>
@@ -32,7 +52,7 @@ const LineChart = (props) => {
                     <option value="month">Month</option>
                 </select>
             </div>
-            <Line data={data} height="190px" />
+            <Line data={dataLine} height="190px" />
         </div>
     );
 };
