4a9eacd8d972d1fbe77934946f706028970ff9ac
KhoiNguyen070401 <khoinguyen070401@gmail.com>
2022-10-16 21:04:00 +0700

    update adminProduct and adminUser

--- components/Admin/Products/Products.js
+++ components/Admin/Products/Products.js
@@ -5,6 +5,7 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.css";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import { Dropdown } from "primereact/dropdown";
 import * as TableServices from './TableServices'
 import Feature from './Feature/Feature';
 import { BsEye } from 'react-icons/bs';
@@ -15,7 +16,10 @@ import {setProductList} from "../../../redux/actions/productAction";
 import {checkStatus} from "./TableServices";
 const styles = {
     wrapper: 'mx-auto w-full p-4 flex flex-col shadow-lg rounded-md',
-    dataTable: 'mt-4'
+    dataTable: 'mt-4',
+    approved: "bg-green-500 px-2 py-1 rounded-lg text-white font-semibold",
+    reject: "bg-red-500 px-2 py-1 rounded-lg text-white font-semibold",
+    pending: "bg-yellow-500 px-2 py-1 rounded-lg text-white font-semibold"
 }
 
 const AdProducts = (props) => {
@@ -26,6 +30,11 @@ const AdProducts = (props) => {
     const [pageInputTooltip, setPageInputTooltip] = useState('Press \'Enter\' key to go to this page.');
     const [selectedProduct, setSelectedProduct] = useState(null)
     const productList = useSelector((state) => AppSelector.getProduct(state));
+    const statuses = [
+        "APPROVED",
+        "PENDING",
+        "REJECT"
+    ];
 
     const onCustomPage = (event) => {
         setFirst(event.first);
@@ -62,6 +71,29 @@ const AdProducts = (props) => {
             Router.push(`/admin/product/update/${path}`)
     }
 
+    const statusBodyTemplate = (rowData) => {
+        return <span className={`customer-badge status-${rowData.status}`}>{rowData.status}</span>;
+    }
+
+    // const statusItemTemplate = (option) => {
+    //     return <span className={`customer-badge status-${option}`}>{option}</span>;
+    // }
+
+    const statusItemTemplate = (option) => {
+        return <span className={`${option}`}>{option}</span>;
+    };
+    
+    const statusRowFilterTemplate = (options) => {
+        return (
+        <Dropdown value={options.value} 
+        options={statuses} 
+        onChange={(e) => options.filterApplyCallback(e.value)} 
+        itemTemplate={statusItemTemplate} 
+        placeholder="Select a Status" 
+     
+        showClear />
+        );
+    }
 
 
     const dispatch = useDispatch();
@@ -78,6 +110,7 @@ const AdProducts = (props) => {
                 <DataTable
                     value={productList}
                     editMode="row"
+                    editable="true"
                     dataKey="id"
                     onRowEditComplete={onRowEditComplete}
                     paginator
@@ -90,6 +123,8 @@ const AdProducts = (props) => {
                     cellSelection
                     selection={selectedProduct}
                     onSelectionChange={onCellSelect}
+                    filterDisplay="row"
+                    showGridlines
                 >
                     <Column
                         field="id"
@@ -98,7 +133,12 @@ const AdProducts = (props) => {
                         filter
                         filterPlaceholder="Search by id"
                         editor={(options) => TableServices.textEditor(options)}
-                        style={{ width: "15%" }}
+                        style={{
+                            width: "20%",
+                            minWidth: "16rem",
+                            padding: "0 0 0 10px",
+                            textAlign: "center"
+                        }}
                     ></Column>
                     <Column
                         field="images"
@@ -108,25 +148,47 @@ const AdProducts = (props) => {
                     <Column
                         field="name"
                         header="Name"
-                        headerStyle={{ width: "30%", minWidth: "8rem" }}
+                        headerStyle={{ width: "20%", minWidth: "8rem" }}
                         sortable
+                        filter
+                        filterPlaceholder="Name"
                         editor={(options) => TableServices.textEditor(options)}
-                        style={{ width: "20%" }}
+                        style={{
+                            width: "20%",
+                            minWidth: "15rem",
+                            padding: "0 0 0 10px",
+                            textAlign: "center"
+                        }}
                     ></Column>
                     <Column
                         field="category"
                         header="Category"
                         sortable
-                        style={{ width: "15%" }}
+                        filter
+                        filterPlaceholder="Category"
+                        style={{
+                            width: "16%",
+                            minWidth: "14rem",
+                            padding: "0 0 0 10px",
+                            textAlign: "center"
+                        }}
+
                     ></Column>
-                    <Column
-                        field="quantity"
-                        header="Status"
-                        headerStyle={{ width: "30%", minWidth: "8rem" }}
-                        sortable
-                        body={TableServices.checkStatus}
-                        editor={(options) => TableServices.statusEditor(options)}
-                    //style={{ width: "50px", padding: "0px"}}
+                    <Column field="status"
+                     header="Status"
+                      showFilterMenu={false}
+                      sortable
+                      filter
+                       filterMenuStyle={{ width: '7rem' }} 
+                       style={{
+                        width: "15%",
+                        minWidth: "6rem",
+                        padding: "5px 0 5px 10px",
+                        textAlign: "center"
+                        }}
+                       editor={(options) => TableServices.statusEditor(options)}
+                       body={statusBodyTemplate} 
+                       filterElement={statusRowFilterTemplate}
                     ></Column>
                     <Column
                         field="price"
@@ -134,7 +196,14 @@ const AdProducts = (props) => {
                         sortable
                         body={TableServices.priceBodyTemplate}
                         editor={(options) => TableServices.priceEditor(options)}
-                        style={{ width: "15%" }}
+                        filter
+                        filterPlaceholder="Price"
+                        style={{
+                            width: "20%",
+                            minWidth: "14rem",
+                            padding: "0 0 0 10px",
+                            textAlign: "center"
+                        }}
                     ></Column>
                     <Column
                         rowEditor
