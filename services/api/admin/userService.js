16ca1195eff69f425fad9e4aee028cb548369440
KhoiNguyen070401 <khoinguyen070401@gmail.com>
2022-10-16 21:04:00 +0700

    update adminProduct and adminUser

--- services/api/admin/userService.js
+++ services/api/admin/userService.js
@@ -1,13 +1,25 @@
-import { collection, getDocs } from "firebase/firestore"
-import { db } from "../../../lib/firebase";
+import { collection, doc, getDocs, updateDoc } from "firebase/firestore"
+import { db } from "../../../lib/firebase"
 
-const userService = { 
-    async getAllUsers () {
-        const ref = await getDocs(collection(db, "users"));
-        return ref.docs.map((doc => {
-            return {...doc.data()}
-        }))
+
+const userService = {
+    async getUsersAll() {
+        const userRef = collection(db, "users");
+        const userSnap = await getDocs(userRef);
+        const usersList = userSnap.docs.map(user => {
+            const data = user.data();
+            return {
+                name: data.name,
+                images: data.imageUrl,
+                email: data.email,
+                address: data.address,
+                phone: data.phone,
+                id: data.id,
+                date: data.userDate,
+            }
+        });
+
+        return usersList;
     },
 }
-
 export default userService;
